7dba4539cafe3feb67df878393b164ac00b25963
samayyy <samaykgada09@gmail.com>
2023-02-04 17:43:59 +0530

    fire-rpc-wallet-connection

--- smart-contract/web3.js
+++ smart-contract/web3.js
@@ -3,15 +3,13 @@ import Web3 from "web3";
 let web3;
 
 if (typeof window !== "undefined" && typeof window.web3 !== "undefined") {
-  // we are in the browser and meta mask is installed
-  web3 = new Web3(window.web3.currentProvider);
+  console.log("we are in the browser and meta mask is installed");
+  web3 = new Web3(window.ethereum);
 } else {
-  // we are on the server *OR* meta mask is not running
-  // creating our own provider
+  console.log("we are on the server *OR* meta mask is not running");
   const provider = new Web3.providers.HttpProvider(
     "https://chain-node.5ire.network/"
   );
-
   web3 = new Web3(provider);
 }
 
--- src/views/SecureFundsView.vue
+++ src/views/SecureFundsView.vue
@@ -50,6 +50,7 @@ export default {
           });
       }
     },
+    disconnect() {},
   },
   watch: {},
 };
