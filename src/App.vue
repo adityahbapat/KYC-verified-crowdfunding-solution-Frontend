0a30581889eaac53f1a0bb525891ff4bb2cbbc7f
samayyy <samaykgada09@gmail.com>
2023-02-04 13:54:53 +0530

    changes

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,33 @@
 <template>
   <v-app>
     <v-main>
-      <router-view/>
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
+  name: "App",
 
   data: () => ({
     //
   }),
+  created() {
+    if (window.ethereum) {
+      this.$store.commit("setWallet", true);
+      window.ethereum.on("accountsChanged", (accounts) => {
+        if (accounts.length > 0) {
+          this.$store.commit("setConnected", true);
+          this.$store.commit("setAccountId", accounts[0]);
+        } else {
+          this.$store.commit("setConnected", false);
+        }
+      });
+    } else {
+      this.$store.commit("setWallet", false);
+    }
+  },
 };
 </script>
 
